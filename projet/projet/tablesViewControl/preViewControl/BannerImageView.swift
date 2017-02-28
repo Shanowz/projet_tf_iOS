3b32f2992d917dd6428aca6b3ed8b6307515ca66
shano <olivier_zimmermann@openmailbox.org>
2017-02-28 15:31:14 +0100

    start doing banner

---
+++ projet/projet/tablesViewControl/preViewControl/BannerImageView.swift
@@ -0,0 +1,22 @@
+//
+//  BannerImageView.swift
+//  projet
+//
+//  Created by student5302 on 28/02/17.
+//  Copyright © 2017 student5302. All rights reserved.
+//
+
+import UIKit
+
+class BannerImageView: UIImageView {
+
+    var bannerImage: UIImage = UIImage(named: "banner")!
+    
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+}
