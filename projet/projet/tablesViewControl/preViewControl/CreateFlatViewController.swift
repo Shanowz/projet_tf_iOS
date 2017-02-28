1d5076ae0d42b2ab2621a136a7662f1b64b708a6
RyGy <remy.grandry95@gmail.com>
2017-02-28 17:08:48 +0100

    petitmessage

--- projet/projet/tablesViewControl/preViewControl/CreateFlatViewController.swift
+++ projet/projet/tablesViewControl/preViewControl/CreateFlatViewController.swift
@@ -8,28 +8,40 @@
 
 import UIKit
 
-class CreateFlatViewController: UIViewController {
+class CreateFlatViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var numberPickerView: UIPickerView!
+    
+    var pickerNumber: [Int] = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.numberPickerView.delegate = self
+        self.numberPickerView.dataSource = self
+        
+        var number: Int = 1
+        for _ in 1...100 {
+            pickerNumber.append(number)
+            number += 1
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerNumber.count
+    }
+    
+    func pickerView(_: UIPickerView, titleForRow: Int, forComponent component: Int) -> String? {
+        let s = NSString(format: "%.0f", pickerNumber[titleForRow])
+        return s as String
+    }
+    
 }
