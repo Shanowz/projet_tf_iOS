ff5c0b3409d97af6e871f34a00db2fc22bee3545
shano <olivier_zimmermann@openmailbox.org>
2017-05-30 11:48:06 +0200

    avant de travailler un petit commit même si j'ai rien fait

---
+++ projet/projet/classes/ConnectionCustomValidateButton.swift
@@ -0,0 +1,22 @@
+//
+//  ConnectionCustomValidateButton.swift
+//  projet
+//
+//  Created by student5302 on 24/05/17.
+//  Copyright © 2017 student5302. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConnectionCustomValidateButton: UIButton{
+    let disabeledButtonColor = UIColor(colorLiteralRed: 200.0/255.0, green: 200.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        super.layer.borderWidth = 1
+        super.layer.cornerRadius = 5
+        super.layer.borderColor = UIColor.darkGray.cgColor
+        super.setTitleColor(disabeledButtonColor, for: UIControlState.disabled)
+        super.setTitleColor(UIColor.darkGray, for: UIControlState.normal)
+    }
+}
--- projet/projet/tablesViewControl/preViewControl/MailLogInViewController.swift
+++ projet/projet/tablesViewControl/preViewControl/MailLogInViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class MailLogInViewController: UIViewController {
     @IBOutlet weak var login_textField: UITextField!
@@ -16,47 +17,38 @@ class MailLogInViewController: UIViewController {
     @IBOutlet weak var pwd_textField: UITextField!
     @IBOutlet weak var confirmPwd_textField: UITextField!
     @IBOutlet weak var validButton: UIButton!
-    @IBOutlet weak var scroll: UIScrollView!
+    
+    var user:User?
     
     let incorrectColor = UIColor(colorLiteralRed: (220.0/255.0), green: (99.0/255.0), blue: (99.0/255.0), alpha: 1.0)
     let normalColor = UIColor(colorLiteralRed: 95.0/255.0, green: 94.0/255.0, blue: 95.0/255.0, alpha: 1.0)
     let disabeledButtonColor = UIColor(colorLiteralRed: 200.0/255.0, green: 200.0/255.0, blue: 200.0/255.0, alpha: 1.0)
     
+    var fields4validation = [UITextField: Bool]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        validButton.layer.borderWidth = 1
-        validButton.layer.cornerRadius = 5
-        validButton.layer.borderColor = UIColor.darkGray.cgColor
-        validButton.setTitleColor(disabeledButtonColor, for: UIControlState.disabled)
-        validButton.setTitleColor(UIColor.darkGray, for: UIControlState.normal)
         
+        fields4validation = [login_textField: false, lastName_textField: false, firstName_textField: false, mail_textField: false, pwd_textField: false, confirmPwd_textField: false]
         
+        //        validButton.layer.borderWidth = 1
+        //        validButton.layer.cornerRadius = 5
+        //        validButton.layer.borderColor = UIColor.darkGray.cgColor
+        //        validButton.setTitleColor(disabeledButtonColor, for: UIControlState.disabled)
+        //        validButton.setTitleColor(UIColor.darkGray, for: UIControlState.normal)
         
-        self.login_textField.addTarget(self, action: #selector(validate(_:)), for: UIControlEvents.editingChanged)
-        self.lastName_textField.addTarget(self, action: #selector(validate(_:)), for: UIControlEvents.editingChanged)
-        self.firstName_textField.addTarget(self, action: #selector(validate(_:)), for: UIControlEvents.editingChanged)
-        self.mail_textField.addTarget(self, action: #selector(validate(_:)), for: UIControlEvents.editingChanged)
-        self.pwd_textField.addTarget(self, action: #selector(validate(_:)), for: UIControlEvents.editingChanged)
-        self.pwd_textField.addTarget(self, action: #selector(validate(_:)), for: UIControlEvents.editingChanged)
-        self.confirmPwd_textField.addTarget(self, action: #selector(validate(_:)), for: UIControlEvents.editingChanged)
+        for (field, _) in self.fields4validation{
+            field.addTarget(self, action: #selector(validate(_:)), for: UIControlEvents.editingChanged)
+        }
         
         disableSubmitButton()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        
-        let size = validButton.frame.origin.y+validButton.frame.size.height
-        
-        let marginB = CGFloat(25)
-        
-        scroll.contentSize = CGSize(width: 0, height: size+marginB)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
     func validate(_ field: UITextField){
         switch field {
@@ -65,74 +57,89 @@ class MailLogInViewController: UIViewController {
                 if let ltext = field.text{
                     UserService.shared.isElementExists(element: "login", value: ltext, handler: {(resp: Int) in
                         if(resp==(-1)){
-                            field.textColor = self.normalColor
-                            print("connection error")
+                            //connection error
                         }else if(resp==1){
-                            field.textColor = self.incorrectColor
-                            //print("element is exist")
+                            self.devalidDaField(field: field)
                         }else if(resp==0){
-                            field.textColor = self.normalColor
+                            self.validDaField(field: field)
                         }
                     })
                 }
                 
             }else{
-                field.textColor = self.incorrectColor
-                //print("invalid caracters")
+                self.devalidDaField(field: field)
             }
             break
             
         case lastName_textField, firstName_textField:
             if(FieldValidation.shared.isValid(fieldText: field.text!, type: FieldValidation.FieldType.name)){
-                field.textColor = self.normalColor
+                self.validDaField(field: field)
             }else{
-                field.textColor = self.incorrectColor
+                self.devalidDaField(field: field)
             }
             break
-        
+            
         case mail_textField:
             if(FieldValidation.shared.isValid(fieldText: field.text!, type: FieldValidation.FieldType.mail)){
-                field.textColor = self.normalColor
-            }else{
-                field.textColor = self.incorrectColor
-            }
-            break
-        
-        case pwd_textField:
-            if(FieldValidation.shared.isValid(fieldText: field.text!, type: FieldValidation.FieldType.pwd)){
                 if let ltext = field.text{
-                    UserService.shared.isElementExists(element: "login", value: ltext, handler: {(resp: Int) in
+                    UserService.shared.isElementExists(element: "mail", value: ltext, handler: {(resp: Int) in
                         if(resp==(-1)){
-                            field.textColor = self.normalColor
-                            print("connection error")
+                            //connection error
                         }else if(resp==1){
-                            field.textColor = self.incorrectColor
-                            //print("element is exist")
+                            self.devalidDaField(field: field)
                         }else if(resp==0){
-                            field.textColor = self.normalColor
+                            self.validDaField(field: field)
                         }
                     })
                 }
+            }else{
+                self.devalidDaField(field: field)
+            }
+            break
+            
+        case pwd_textField:
+            if(FieldValidation.shared.isValid(fieldText: field.text!, type: FieldValidation.FieldType.pwd)){
+                self.validDaField(field: field)
+            }else{
+                self.devalidDaField(field: field)
             }
             break
             
         case confirmPwd_textField:
             if(field.text == pwd_textField.text){
-                field.textColor = self.normalColor
+                self.validDaField(field: field)
             }else{
-                field.textColor = self.normalColor
+                self.devalidDaField(field: field)
             }
-            
             break
             
         default:
-            
             break
         }
-
+        
+        if(is_all_valid()){
+            enableSubmitButton()
+        }else{
+            disableSubmitButton()
+        }
     }
-
+    
+    func validDaField(field: UITextField){
+        field.textColor = self.normalColor
+        fields4validation[field] = true
+    }
+    
+    func devalidDaField(field: UITextField){
+        field.textColor = self.incorrectColor
+        fields4validation[field] = false
+    }
+    
     func is_all_valid() -> Bool{
+        for (field, isValid) in self.fields4validation{
+            if(!isValid){
+                return false
+            }
+        }
         return true
     }
     
@@ -146,14 +153,24 @@ class MailLogInViewController: UIViewController {
         validButton.isEnabled = false
     }
     
-    func setTextRed(field: UITextField){
-        if((field.text?.characters.count)! > 0){
-            field.textColor = self.incorrectColor
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "user_profil" {
+            if let viewController = segue.destination as? UserProfileViewController{
+                viewController.user = User(login: login_textField.text!, pwd: pwd_textField.text!, mail: mail_textField.text!, fname: firstName_textField.text!, lname: lastName_textField.text!)
+            }
         }
     }
     
-    
-    @IBAction func createUser_Button(_ sender: Any) {
-    
+    @IBAction func createUser(_ sender: Any) {
+        let user = User(login: login_textField.text!, pwd: pwd_textField.text!, mail: mail_textField.text!, fname: firstName_textField.text!, lname: lastName_textField.text!)
+        UserService.shared.createUser(json: user.toJSON(), handler: {(resp: Bool) in
+            if resp{
+                print("user has been created")
+            }else{
+                print("can't create the user")
+            }
+        })
     }
 }
+
+
